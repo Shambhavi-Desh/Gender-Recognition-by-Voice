aa697155ce7388abe97e527c34406549ee7262ce
Shambhavi-Desh <65267474+Shambhavi-Desh@users.noreply.github.com>
2020-05-13 11:00:22 +0530

    Charac_features.m

---
+++ Charac_features.m
@@ -0,0 +1,24 @@
+feature_table.fundamental_freq(window, 1) = fundamental_freq;    
+   %store fundamental frequency in a new column of the table   
+   MFCC =                                                                                                                                                           
+{'MFCC1','MFCC2','MFCC3','MFCC4','MFCC5','MFCC6','MFCC7','MFCC8','MFCC9','MFCC10','MFCC11','MFCC12'};
+   for k=1:12
+       feature_table.(MFCC{k})(window, 1) = coeffs(k);  
+       %store MFCC coefficients from 1 to 12 in separate table columns
+   end
+end
+
+disp(feature_table)     %display table contains extracted features
+writetable(feature_table,'features.xls');   %write table to file
+% Testing dataset is ready. Similarly find training dataset
+for i=1:n % 30 millisecond of the frame and 10 millisecond of the overlapping
+    if i==1
+        sample(:,1) = recording(1:num_of_Samples);
+    else
+        sample(:,i) = 
+        recording(num_Samp*(i-1)+1:num_Samp*(i-1)+num_of_Samples);
+    end
+end
+
+
+
